8dfc07de5ddacc2228a9c0d661bd04739486c484
Eirik Vågeskar <eirik.vageskar@protonmail.com>
2022-02-17 08:38:36 +0100

    :wrench: Turn on eslint rule curly

--- .xo-config.js
+++ .xo-config.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     ...SORT_KEYS_RULES,
     ...getJsdocRules(true),
+    curly: "error",
     "multiline-comment-style": ["error", "starred-block"],
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
